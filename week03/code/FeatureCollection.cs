46471d362b6f3ff385a8b71ad0b685c2d1882e25
Hernan Rodriguez <hernan@covr.care>
2024-03-15 13:19:04 +0100

    add earthquake solution

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,4 +1,56 @@
 public class FeatureCollection {
     // Todo Earthquake Problem - ADD YOUR CODE HERE
     // Create additional classes as necessary
+    public string type { get; set; }
+    public Metadata metadata {get; set;}
+    public Feature[] features { get; set; }
+}
+
+public class Feature {
+    public string type { get; set; }
+    public FeatureProperties properties { get; set; }
+    public Geometry geometry { get; set; }
+    public string id { get; set; }
+}
+public class FeatureProperties {
+    public double mag { get; set; }
+    public string place { get; set; }
+    public long time { get; set; }
+    public long updated { get; set; }
+    public object tz { get; set; }
+    public string url { get; set; }
+    public string detail { get; set; }
+    public object felt { get; set; }
+    public object cdi { get; set; }
+    public object mmi { get; set; }
+    public object alert { get; set; }
+    public string status { get; set; }
+    public int tsunami { get; set; }
+    public int sig { get; set; }
+    public string net { get; set; }
+    public string code { get; set; }
+    public string ids { get; set; }
+    public string sources { get; set; }
+    public string types { get; set; }
+    public int? nst { get; set; }
+    public double? dmin { get; set; }
+    public double? rms { get; set; }
+    public double? gap { get; set; }
+    public string magType { get; set; }
+    public string type { get; set; }
+    public string title { get; set; }
+}
+public class Geometry 
+{
+    public string type { get; set; }
+    public List<double> coordinates { get; set; }
+}
+
+public class Metadata {
+    public long generated;
+    public string url;
+    public string title;
+    public string api;
+    public int count;
+    public int status;
 }
--- week03/code/SetsAndMapsTester.cs
+++ week03/code/SetsAndMapsTester.cs
@@ -288,13 +288,19 @@ public static class SetsAndMapsTester {
         using var jsonStream = client.Send(getRequestMessage).Content.ReadAsStream();
         using var reader = new StreamReader(jsonStream);
         var json = reader.ReadToEnd();
+
         var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
         var featureCollection = JsonSerializer.Deserialize<FeatureCollection>(json, options);
-
         // TODO:
         // 1. Add code in FeatureCollection.cs to describe the JSON using classes and properties 
         // on those classes so that the call to Deserialize above works properly.
         // 2. Add code below to print out each place a earthquake has happened today and its magitude.
+        foreach (var earthquake in featureCollection.features)
+        {
+            string place = earthquake.properties.place;
+            double magnitude = earthquake.properties.mag;
+            Console.WriteLine($"{place} - Mag {magnitude.ToString()}");
+        }
     }
 }
